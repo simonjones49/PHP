d72c20a2e7f94ccf16ab01d2a6599b632904a606
Simon <simon_jones49@gmx.com>
2021-08-05 11:10:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,32 +21,77 @@
  * 
  * 
  */
-$filedir = "./files994";
-$file = "$_post[fileid]";
-$file = 1;
-if (preg_match('#[0-9]#',$file)){
+// Set dir to use 
+$filedir = './files994';
+
+// scan for file
+$filelist = scandir($filedir);
+
+// Check if we have input for a file
+$filename = $_GET['file'];
+
+// Check filename is not empty but it is set
+if (isset($filename) && $filename !== ''){	
+$dl_type="application/octet-stream";
+	
+$path = $filedir . '/' . $filename;
+
+if(file_exists($path)) {
+	
+header("Pragma: public");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Type: " . $dl_type);
+header("Content-Length: " .(string)(filesize($path)) );
+header('Content-Disposition: attachment; filename="'.basename($path).'"');
+header("Content-Transfer-Encoding: binary\n");
+
+readfile($path); // outputs the content of the file
+
+
+//Terminate from the script
+die();
+}
+else{
+// We get here if the filename exists but the file does not	
+echo "File does not exist.";
+}
+
+
+}
+else {
+// Front page 
+
 echo '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<title>untitled</title>
+	<title>Downloads</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="generator" content="Geany 1.37.1" />
 </head>
 
 <body>
+';
+
+foreach($filelist as $item):
 	
+	if (preg_match('#[a-z]#',$item)){
+		
+		echo '<p><a href="index.php?file=' . $item . '">' . $item . '</a></p>';
+		
+	}
+endforeach;
+
+echo '	
 </body>
 
 </html>
 
 ';
-	
-}
-else {
-	
-	
 }
 ?>
